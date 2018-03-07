3523cb1be970f2cde0ce0a083ab401ecce11ab5e
Brian Caterfino <bcaterfino@gmail.com>
2018-03-07 19:00:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,18 +4,17 @@ return string.toUpperCase()
 
 
 function logshout(string)
- console.log string.toLowerCase()
+ console.log (string.toLowerCase)()
 
 
 
 function whisper(string) {
-  
 return string.toLowerCase()
 }
 
 
 function logwhisper(string)
- console.log string.toUpperCase()
+ console.log (string.toUpperCase)()
  
  
 
